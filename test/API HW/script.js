9ae25f99d2b5a51d25aa974280e57fbd1a0e3238
haemn <kwon568@newschool.edu>
2015-10-12 20:33:41 -0400

    HW3

---
+++ test/API HW/script.js
@@ -0,0 +1,25 @@
+// console.log("hello")
+
+// // var tagNameVal = $("#tags").val();
+
+// console.log('tagNameVal');
+
+
+// 		var feed = new Instafeed({
+// 			get: 'tagged',
+// 			tagName: 'selfie',
+// 			// tagName: tagNameVal,
+// 			limit:'32',
+// 			clientId: 'fb88d4cd528c4097a3c34ebb9cf80f6b'
+// 		});
+
+
+// 		$( document ).ready(function() {
+// 		    $("#submitBtn").click(function() {
+// 				// var tagNameVal;
+// 				// var tagNameVal = $('#tags').val();
+// 				// console.log();
+// 				feed.run(); 
+// 			});
+// 		});
+
